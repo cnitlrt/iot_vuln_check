3f16dfb4887fb32d1307a00c6491d6347d0b4537
cnitlrt <60732685+cnitlrt@users.noreply.github.com>
2023-11-22 05:36:21 +0800

    Update check.py

--- check.py
+++ check.py
@@ -7,6 +7,7 @@ class Success(Enum):
     FORMAT_OVERFLOW = auto()
     STACKOVERFLOW = auto()
     COMMANDINJECT = auto()
+    COMMANDUNCONSTANT = auto()
     BUFOVERFLOW = auto()
 #check command injection
 def check_commend(symbol):
@@ -22,6 +23,37 @@ def check_commend(symbol):
         callees = func.callees        
         if 'sprintf' in [callee.symbol.name for callee in callees]:        
             ret.append((symbol,func.name,ref.address,Success.COMMANDINJECT))
+
+def check_doSystem(symbol):
+    addr = get_function_addr(symbol)
+    if addr == None:
+        return []
+    refs = bv.get_code_refs(addr)
+    for ref in refs:
+        func = ref.function
+        fmt = func.get_parameter_at(ref.address,None,0)
+        if not is_constant(fmt):
+            ret.append((symbol,func.name,ref.address,Success.COMMANDUNCONSTANT))
+            continue
+        asc = bv.get_ascii_string_at(fmt.value,min_length = 2)
+        if asc == None:
+            continue
+        fmt_value = asc.value
+        cidx = 0
+        arg_idx = 0
+        while True:
+            idx = fmt_value.find("%",cidx)
+            if idx < 0:
+                break
+            arg_idx += 1
+            cidx = idx + 1
+            if fmt_value[idx:].startswith("%s"):
+                arg = func.get_parameter_at(ref.address,None,arg_idx)
+                if not is_constant(arg):
+                    ret.append((symbol,func.name,ref.address,Success.COMMANDINJECT))
+                    break
+            
+
 def check_execv(symbol = "execv"):
     addr = get_function_addr(symbol)
     if addr == None:
@@ -31,9 +63,7 @@ def check_execv(symbol = "execv"):
         func = ref.function
         cmd = func.get_parameter_at(ref.address,None,1)
         if is_constant(cmd):
-            continue
-        # callees = func.callees        
-        # if 'sprintf' in [callee.symbol.name for callee in callees]:        
+            continue    
         ret.append((symbol,func.name,ref.address,Success.COMMANDINJECT))
 
 def check_sprintf(symbol = "sprintf"):
@@ -63,6 +93,7 @@ def check_sprintf(symbol = "sprintf"):
                 arg = func.get_parameter_at(ref.address,None,arg_idx)
                 if not is_constant(arg):
                     ret.append((symbol,func.name,ref.address,Success.FORMAT_OVERFLOW))
+                    break
 
 def check_printf(symbol = "printf"):
     addr = get_function_addr(symbol)
@@ -89,12 +120,14 @@ def check_syslog(symbol = "syslog"):
 
 def check_strcpy(symbol = "strcpy"):
     addr = get_function_addr("strcpy")
+    # print(hex(addr))
     if addr == None:
         return []
     refs = bv.get_code_refs(addr)
     for ref in refs:
         func = ref.function
         buf = func.get_parameter_at(ref.address,None,0)
+        # print(buf.type)
         if buf.type == RegisterValueType.StackFrameOffset:    
             buf = func.get_parameter_at(ref.address,None,1)
             ret.append((symbol,func.name,ref.address,Success.BUFOVERFLOW))
@@ -144,16 +177,23 @@ def check_overflow():
 
 def check_cmd():
     check_commend("system")
+    check_doSystem("doSystem")
     check_commend("popen")
     check_commend("execve")
     check_execv()
-    
 
 def get_function_addr(symbol):
     syms = []
-    syms = bv.symbols[symbol]
-    for x in syms:
-        return x.address
+    if symbol in bv.symbols:
+        syms = bv.symbols[symbol]
+    for i in syms:
+        if "mips32" == bv.arch.name or "mipsel32" == bv.arch.name:
+            if i.type == SymbolType.ImportAddressSymbol:
+                return i.address
+        else:
+            if i.type == SymbolType.ImportedFunctionSymbol:
+                return i.address
+    return None
 
 def is_constant(a):
     return a.type == RegisterValueType.ConstantPointerValue or a.type == RegisterValueType.ConstantValue
